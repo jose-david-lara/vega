d5c03a2db2b5ef7cdc6ea94b962808126022b38b
Jose Lara <jose@hotmail.com>
2022-08-03 23:37:21 -0400

    Commit * EurekA Server

--- service-user/src/main/java/com/vega/user/ServiceUserApplication.java
+++ service-user/src/main/java/com/vega/user/ServiceUserApplication.java
@@ -2,8 +2,10 @@ package com.vega.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceUserApplication {
 
 	public static void main(String[] args) {
